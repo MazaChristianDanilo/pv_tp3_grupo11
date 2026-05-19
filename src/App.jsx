42fdd0b8ad24b951b39e0a72d60a39c22c7921a8
Danilo Christian Maza <danchr2002@gmail.com>
2026-05-19 19:15:32 -0300

    Arreglo el archivo App

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,11 @@ import Nav from './components/Nav';
 import Footer from './components/Footer';
 
 const App = () => {
-    const [proyectos, setProyectos] = useState([
-      {
-        id: 1,
-        titulo: 'Proyecto Desarrollo Web',
-        categoria: 'Desarrollo Web',
-        estado: 'En Progreso',
-      },
-      {
-        id: 2,
-        titulo: 'Proyecto MArketing',
-        categoria: 'Marketing Digital',
-        estado: 'Completado',
-      },
-    ]);
-
-    const eliminarProyecto = (id) => {
-        setProyectos(proyectos.filter(proyecto => proyecto.id !== id));
-    };
-
-    const verDetalle = (proyecto) => {
-      console.log(proyecto);
-    };
-
     return (
         <>
           <Header />
           <Nav />
-          <ListaProyectos proyectos={proyectos} eliminarProyecto={eliminarProyecto} verDetalle={verDetalle} />
+          <ListaProyectos/>
           <Footer />
         </>
       );
