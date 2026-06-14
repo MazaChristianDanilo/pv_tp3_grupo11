70d840108d62a423c4364a75c55fd25b381435f8
TatianaYurquina <tatianayurquina6@gmail.com>
2026-06-14 03:44:44 -0300

    Agrego modificación de perfil con context API

---
+++ src/context/UsuarioContext.jsx
@@ -0,0 +1,22 @@
+export const UsuarioContext = createContext();
+import { createContext, useState } from "react";
+export const UsuarioProvider = ({ children }) => {
+  const [usuario, setUsuario] = useState(() => {
+    return {
+      nombre: "Juan Perez",
+      dni: "26167892",
+      rol: "Docente",
+      institucion: "Universidad Nacional de Jujuy",
+    };
+  });
+
+  const actualizarPerfil = (datosActualizados) => {
+    setUsuario(datosActualizados);
+  }
+
+  return (
+    <UsuarioContext.Provider value={{ usuario, actualizarPerfil }}>
+      {children}
+    </UsuarioContext.Provider>
+  );
+};
